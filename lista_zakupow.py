cc9e4506f3aaf6ae6f5ee5a4de503e46913270f0
Anna Wamej-Stolc <a.wamej@gmail.com>
2020-10-25 17:50:20 +0100

    Change capitalized

--- lista_zakupow.py
+++ lista_zakupow.py
@@ -1,5 +1,9 @@
 print('Lista zakupów')
 lista_zakupow = {'piekarnia' : ['chleb', 'bułki', 'pączek'], 'warzywniak' : ['marchew', 'seler', 'rukola']}
 for store_name,products in lista_zakupow.items():
-    products_to_print = ', '.join(products)
-    print("Idę do "+ store_name +" i kupuję tam: " + products_to_print)
+    store_name_capitalize = store_name.capitalize()
+    capitalized_products = []
+    for i in products:
+        capitalized_products.append(i.capitalize())
+    products_to_print = ', '.join(capitalized_products)
+    print("Idę do "+ store_name_capitalize +" i kupuję tam: " + products_to_print)
