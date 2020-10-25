8863c0e0a2ecdbf6b34e2a652664e10c7b890e5d
Anna Wamej-Stolc <a.wamej@gmail.com>
2020-10-25 17:48:32 +0100

    Add print

--- lista_zakupow.py
+++ lista_zakupow.py
@@ -1,2 +1,5 @@
 print('Lista zakupów')
 lista_zakupow = {'piekarnia' : ['chleb', 'bułki', 'pączek'], 'warzywniak' : ['marchew', 'seler', 'rukola']}
+for store_name,products in lista_zakupow.items():
+    products_to_print = ', '.join(products)
+    print("Idę do "+ store_name +" i kupuję tam: " + products_to_print)
